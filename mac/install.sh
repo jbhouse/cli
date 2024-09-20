43185e2b29e488daa214a53d2b34e15aeb25596d
jbhouse <jbmoorhouse1@gmail.com>
2024-09-19 21:30:15 -0600

    master: updating install script

--- mac/install.sh
+++ mac/install.sh
@@ -15,6 +15,8 @@ echo 'install openjdk 11? (y/Y) or (n/N)'
 read install11
 echo 'install openjdk 17? (y/Y) or (n/N)'
 read install17
+echo 'install openjdk 21? (y/Y) or (n/N)'
+read install21
 echo '----------------------------'
 
 if [[ "${install8}" == "Y" ]] || [[ "${install8}" == "y" ]]
@@ -32,6 +34,14 @@ fi
 
 if [[ "${install17}" == "Y" ]] || [[ "${install17}" == "y" ]]
   then brew install openjdk@17
+  # && sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+  # intel
+  # && sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+  # m1
+fi
+
+if [[ "${install21}" == "Y" ]] || [[ "${install21}" == "y" ]]
+  then brew install openjdk@21
   # && sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
   # intel
   # && sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
